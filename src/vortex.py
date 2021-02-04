1e03606626c92af83ec551b8b089fffbfdbbdd64
angelinag <andji.gash@gmail.com>
2021-02-04 12:50:09 +0200

    Added basic .ENV parser

--- src/vortex.py
+++ src/vortex.py
@@ -1,7 +1,11 @@
+from dotenv import find_dotenv
+import os
+
+
 class Vortex:
     def __init__(self):
         self.SECRETS = {}   # Populate with sources
-        self.endpoints = {} # Populate with sinks
+        self.endpoints = {}  # Populate with sinks
 
     def get_yml(self):
         '''
@@ -13,5 +17,27 @@ class Vortex:
         '''
         Parse .env files and populate secrets
         '''
-        pass
+        try:
+            dotenv_path = find_dotenv()
+            dotenv_file = open(dotenv_path, 'r')
+            contents = dotenv_file.readlines()
+            for line in contents:
+                variable = line.split("=")
+                key = variable[0]
+                value = variable[1]
+                if value == 'true':
+                    value = True
+                elif value == 'false':
+                    value = False
+                else:  # TODO: Define other types parsing OR use default load_dotenv
+                    value = value.replace('\n', '')
+                    value = value.replace('"', '')
+                self.SECRETS[key] = value
+                print(self.SECRETS)
+        except:
+            print(".env file not found in specified location.")
+
+
+v = Vortex()
+v.get_env()
 
